244ac4072fa7507be75fb3e85a4a99af3576a317
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2021-02-17 23:41:28 +0100

    added test for API response

---
+++ src/server/_test_/mockAPI.js
@@ -0,0 +1,7 @@
+let json = {
+     'title': 'I am testing the api call',
+     'description': 'mocking json Api request',
+     'message': 'Attention: Request is successful'
+};
+
+module.exports = json;
